96ecfbccf35ca198d6001ad164b441cd3fd98db8
ruki <waruqi@gmail.com>
2021-01-17 18:40:18 +0800

    add debug info

--- xmake.lua
+++ xmake.lua
@@ -39,6 +39,7 @@ rule("luarocks.module")
             if not result then
                 local config = os.iorun("luarocks config")
                 if config then
+                    print(config)
                     local LUALIB     = config:match("LUALIB = \"(.-)\"")
                     local LUA_INCDIR = config:match("LUA_INCDIR = \"(.-)\"")
                     local LUA_LIBDIR = config:match("LUA_LIBDIR = \"(.-)\"")
@@ -52,6 +53,7 @@ rule("luarocks.module")
                     detectcache:save()
                 end
             end
+            print(result)
             return result
         end}
         if not lua then
