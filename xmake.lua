fff2c9ef8b3218221965681e1b2ca427be321c88
ruki <waruqi@gmail.com>
2021-01-17 20:06:34 +0800

    remove lua library

--- xmake.lua
+++ xmake.lua
@@ -32,38 +32,6 @@ rule("luarocks.module")
         else
             target:set("symbols", "none")
         end
-
-        -- find lua library
-        local lua = try {function ()
-            local result = detectcache:get("luarocks.module.lua")
-            if not result then
-                local config = os.iorun("luarocks config")
-                if config then
-                    local LUALIB     = config:match("LUALIB = \"(.-)\"")
-                    local LUA_INCDIR = config:match("LUA_INCDIR = \"(.-)\"")
-                    local LUA_LIBDIR = config:match("LUA_LIBDIR = \"(.-)\"")
-                    if LUALIB and LUA_INCDIR and LUA_LIBDIR then
-                        result = result or {}
-                        result.links = project_target.linkname(LUALIB)
-                        result.linkdirs = {LUA_LIBDIR}
-                        result.includedirs = {LUA_INCDIR}
-                    end
-                    detectcache:set("luarocks.module.lua", result)
-                    detectcache:save()
-                end
-            end
-            return result
-        end}
-        if not lua then
-            lua = find_package("lua")
-        end
-        if lua then
-            -- we need not link lua explicitly
-            if target:is_plat("macosx", "linux", "bsd") then
-                lua.links = nil
-            end
-            target:add(lua)
-        end
     end)
     on_install(function (target)
         local moduledir = path.directory((target:name():gsub('%.', '/')))
