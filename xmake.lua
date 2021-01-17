1830841371c04144afdabc174cd38065e5dc6e54
ruki <waruqi@gmail.com>
2021-01-17 15:48:27 +0800

    improve xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,37 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("lua")
-target("example1.hello")
-    if is_plat("macosx") then
-        set_kind("binary")
-        set_filename("hello.so")
-        add_ldflags("-bundle", "-undefined dynamic_lookup", {force = true})
-    else
-        set_kind("shared")
-        set_basename("hello")
-    end
-    set_symbols("none")
-    add_files("src/test.c")
-    add_packages("lua")
+rule("luarocks.module")
+    before_load(function (target)
+
+        -- set kind
+        if target:is_plat("macosx") then
+            target:set("kind", "binary")
+            target:add("ldflags", "-bundle", "-undefined dynamic_lookup", {force = true})
+        else
+            target:set("kind", "shared")
+        end
+
+        -- set library name
+        local modulename = target:name():split('.', {plain = true})
+        modulename = modulename[#modulename]
+        if target:is_plat("macosx") then
+            target:set("filename", modulename .. ".so")
+        else
+            target:set("basename", modulename)
+        end
+
+        -- export symbols, TODO def
+        target:set("symbols", "none")
+    end)
     on_install(function (target)
         local moduledir = path.directory((target:name():gsub('%.', '/')))
         import('target.action.install')(target, {libdir = path.join('lib', moduledir), bindir = path.join('lib', moduledir)})
     end)
+rule_end()
+
+add_requires("lua")
+target("example1.hello")
+    add_rules("luarocks.module")
+    add_files("src/test.c")
+    add_packages("lua")
 
